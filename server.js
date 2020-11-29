2d6a81115aa8918d6f42d0ed66bd3ed5d9ef5587
Adam Barron <adambarron@me.com>
2020-11-29 15:36:31 -0800

    added get route

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const { notes } = require('./db/db.json')
 
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
-})
+    res.json(notes); 
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
