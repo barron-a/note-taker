b9e8267e7a1e10e6c2edf458874740482da54c43
Adam Barron <adambarron@me.com>
2020-11-29 20:51:24 -0800

    tested new notes writing and saving in Insomnia core

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 const { notes } = require('./db/db.json')
 
 function createNewNote(body, notesArray) {
@@ -38,6 +39,14 @@ app.post('/api/notes', (req, res) => {
         const note = createNewNote(req.body, notes);
         res.json(note);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.listen(PORT, () => {
