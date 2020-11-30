83adb87a9b9059387e3637392e49192fcb0dd835
Adam Barron <adambarron@me.com>
2020-11-29 17:34:41 -0800

    added more functionality to the get and post methods along with createNewNote function and validateNote function (not yet fully working)

--- server.js
+++ server.js
@@ -1,17 +1,43 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { nanoid } = require('nanoid');
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const { notes } = require('./db/db.json')
 
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+};
+
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    return true;
+}
+
 app.get('/api/notes', (req, res) => {
     res.json(notes); 
 });
 
 app.post('/api/notes', (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
+    req.body.id = nanoid();
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
 });
 
 app.listen(PORT, () => {
